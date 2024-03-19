cf9646ba1dcda5f8e4749a35123d28e8824dcf12
OzarkOC <ozarkoutpostcenter@gmail.com>
2024-03-19 17:45:25 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+import { retrieveData } from "./scripts/userData";
+// const p = document.querySelector("p");
+
+// const data = async function () {
+//   const d = await retrieveData("oocUser", "deavarae@gmail.com");
+//   p.textContent = JSON.stringify(d, null, 2);
+// };
+
+// data();
