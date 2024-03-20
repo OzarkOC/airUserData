9533f961222bfaba32f060f7e5378a2ce3df4bbd
OzarkOC <ozarkoutpostcenter@gmail.com>
2024-03-19 20:23:41 -0500

    changed attribute name

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { retrieveData } from "./scripts/userData";
 window.Webflow ||= [];
 window.Webflow.push(() => {
   // ATTRIBUTES
-  const userEmail = document.querySelector(".userdata").getAttribute("uEmail");
+  const userEmail = document.querySelector(".userdata").getAttribute("uemail");
   console.log(userEmail);
   // MARKUPS
 });
